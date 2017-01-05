e9a276951f09b50fc5f2f6cbd3d9cbba75b6888d
Rob Nelson <rnelson0@gmail.com>
2017-01-05 22:30:05 +0000

    Remove pe_gem, remove references to pe_gem, and some other Puppet 3 blocks and tests

--- spec/classes/webhook/package_spec.rb
+++ spec/classes/webhook/package_spec.rb
@@ -6,7 +6,7 @@ describe 'r10k::webhook::package', type: :class do
         facts
       end
 
-      context 'Puppet Enterprise 2015.x' do
+      context 'Puppet Enterprise 2016.4.2' do
         let :params do
           {
             is_pe_server: true
@@ -14,8 +14,8 @@ describe 'r10k::webhook::package', type: :class do
         end
         let :facts do
           facts.merge(
-            is_pe:         'true',
-            puppetversion: '4.2.0'
+            pe_server_build: '2016.4.2',
+            puppetversion:   '4.2.0'
           )
         end
 
@@ -28,59 +28,6 @@ describe 'r10k::webhook::package', type: :class do
         it { is_expected.to contain_package('rack').with(ensure: 'installed') }
         it { is_expected.not_to contain_package('webrick') }
         it { is_expected.not_to contain_package('json') }
-      end
-
-      context 'Puppet Enterprise 3.6' do
-        let :facts do
-          facts.merge(
-            is_pe:         true,
-            puppetversion: '3.6.0'
-          )
-        end
-
-        it do
-          is_expected.to contain_package('sinatra').with(
-            ensure:   'installed',
-            provider: 'pe_gem'
-          )
-        end
-        it { is_expected.not_to contain_package('rack') }
-        it { is_expected.not_to contain_package('webrick') }
-        it { is_expected.not_to contain_package('json') }
-      end
-
-      context 'Puppet FOSS 3.x' do
-        let :facts do
-          facts.merge(
-            is_pe:          false,
-            puppetversion: '3.8.0'
-          )
-        end
-
-        it do
-          is_expected.to contain_package('sinatra').with(
-            ensure:   'installed',
-            provider: 'gem'
-          )
-        end
-        it do
-          is_expected.to contain_package('webrick').with(
-            ensure:   'installed',
-            provider: 'gem'
-          )
-        end
-        it do
-          is_expected.to contain_package('json').with(
-            ensure:   'installed',
-            provider: 'gem'
-          )
-        end
-        it do
-          is_expected.to contain_package('rack').with(
-            ensure:   'installed',
-            provider: 'gem'
-          )
-        end
       end
 
       context 'Puppet FOSS 4.x' do
